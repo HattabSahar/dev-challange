1a564924ba9d4b6f962ab9f257c835921bedbecb
Sahar Hattab <hattab.sahaar@gmail.com>
2022-04-21 14:40:50 +0100

    Init mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
-const { PORT } = process.env
+import mongoose from 'mongoose'
+const { MONGO_DB_URI, PORT } = process.env
 const app = express()
 
-const port = PORT || 8080
-app.listen({ port }, () => {
-  console.log(`Server running on port ${port}`)
+mongoose.set('useCreateIndex', true)
+mongoose.connect(MONGO_DB_URI, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  const port = PORT || 8080
+  app.listen({ port }, () => {
+    console.log(`Server running on port ${port}`)
+  })
 })
+mongoose.connection.on('error', error => console.error(error))
