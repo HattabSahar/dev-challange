b32445c915a0c34be0ccec55cc6fd16a62868240
Sahar Hattab <hattab.sahaar@gmail.com>
2022-04-21 17:53:42 +0100

    Make home page

---
+++ client/src/pages/Dashboard/Home.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import DashboardLayout from '../../Layouts/DashboardLayout'
+
+function Home() {
+  return (
+    <DashboardLayout isHero>
+      <q className='text-center display-3'>
+        Your opinion matters
+        <span className='d-block'>&</span>
+        Your idea inspires
+      </q>
+
+      <div className='h5 d-flex align-items-center' style={{ gap: 15 }}>
+        <Link className='text-secondary' to='/about-us'>
+          About us
+        </Link>
+        <small style={{ fontSize: 10 }}>⬤</small>
+        <Link className='text-secondary' to='/goal'>
+          Goal
+        </Link>
+        <small style={{ fontSize: 10 }}>⬤</small>
+        <Link className='text-secondary' to='/contact'>
+          Contact
+        </Link>
+      </div>
+    </DashboardLayout>
+  )
+}
+
+export default Home
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import CommingSoon from './CommingSoon'
+import Home from './Home'
 
 function HomePage() {
   return (
     <Routes>
-      <Route index element={<CommingSoon />}></Route>
+      <Route index element={<Home />}></Route>
       <Route path='ideas' element={<CommingSoon />}></Route>
       <Route path='discussion' element={<CommingSoon />}></Route>
       <Route path='project' element={<CommingSoon />}></Route>
